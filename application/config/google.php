48ee754add198900c183d952817e6c6ace56603b
rafiadji <rafiadji13@gmail.com>
2018-05-04 10:15:53 +0700

    Pertama

---
+++ application/config/google.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['google']['application_name'] = 'PPK2018App';
+$config['google']['client_id'] = '424148071662-0aasjbpl6rn1osken5notg1qghpompf3.apps.googleusercontent.com';
+$config['google']['client_secret'] = 'YRychBkkt5zSeh8XJleY2YKm';
+$config['google']['redirect_uri'] = 'http://localhost/PPK2018/';
+$config['google']['api_key'] = 'AIzaSyAaQ9OZ4yYjJ7EZk5o_1ORNKCLxYgTKWJU';
